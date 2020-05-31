9477a3b3a05499b0125c8a47fd2f4d52f28961df
saynschuman <saynschuman@gmail.com>
2020-05-31 14:19:06 +0300

    modals finished

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,10 @@ import randomInt from "../utils/randomInt";
 const App = () => {
   const [locationIndex, setLocationIndex] = React.useState(0);
   const [locationIsVisible, setLocationVisibility] = React.useState(false);
-  const [players, setPlayers] = React.useState(10);
+  const [players, setPlayers] = React.useState(2);
+  const [currentPlayer, setCurrentPlayer] = React.useState(0);
+  const [hintIsVisible, setHintVisibility] = React.useState(false);
+  const [timerIsOpen, setTimerIsOpen] = React.useState(false);
   return (
     <Container className="pt-3">
       <h4 className="text-center mb-4">Выберите количество игроков</h4>
@@ -29,8 +32,7 @@ const App = () => {
           <Button
             color="secondary"
             onClick={() => {
-              setLocationIndex(randomInt(0, locations.length));
-              setLocationVisibility(true);
+              setHintVisibility(true);
             }}
           >
             Начать
@@ -38,38 +40,52 @@ const App = () => {
         </InputGroupAddon>
       </InputGroup>
       <Modal isOpen={locationIsVisible} className="text-center">
-        {!locationIsVisible ? (
-          <>
-            <ModalHeader className="w-100 d-block">
-              Передай телефон первому игроку
-            </ModalHeader>
-            <ModalBody>Нажми ок когда будешь готов</ModalBody>
-            <Button
-              color="primary"
-              className="m-2"
-              onClick={() => setLocationVisibility(true)}
-            >
-              Ok
-            </Button>
-          </>
-        ) : (
-          <>
-            <ModalHeader className="w-100 d-block">
-              {locations[locationIndex]}
-            </ModalHeader>
-            <ModalBody className="text-justify">
-              Ты местный. <br /> Все игроки кроме Иуды знают эту локацию.
-              Задавай вопросы игрокам, чтобы вычислить кто из них Иуда.
-            </ModalBody>
-            <Button
-              color="primary"
-              className="m-2"
-              onClick={() => setLocationVisibility(false)}
-            >
-              Ok
-            </Button>
-          </>
-        )}
+        <ModalHeader className="w-100 d-block">
+          {locations[locationIndex]}
+        </ModalHeader>
+        <ModalBody className="text-justify">
+          Ты местный. <br /> Все игроки кроме Иуды знают эту локацию. Задавай
+          вопросы игрокам, чтобы вычислить кто из них Иуда.
+        </ModalBody>
+        <Button
+          color="primary"
+          className="m-2"
+          onClick={() => {
+            setHintVisibility(true);
+            setLocationVisibility(false);
+          }}
+        >
+          Ok
+        </Button>
+      </Modal>
+      <Modal isOpen={hintIsVisible} className="text-center">
+        <ModalHeader className="w-100 d-block">
+          Передай телефон {currentPlayer === 0 ? "первому" : "следующему"}
+          <span className="pl-1">игроку</span>
+        </ModalHeader>
+        <ModalBody>Нажми ок когда будешь готов</ModalBody>
+        <Button
+          color="primary"
+          className="m-2"
+          // onClick={() => setLocationVisibility(true)}
+          onClick={() => {
+            !currentPlayer && setLocationIndex(randomInt(0, locations.length));
+            if (players === currentPlayer) {
+              setTimerIsOpen(true);
+              setHintVisibility(false);
+              setCurrentPlayer(0);
+            } else {
+              setCurrentPlayer(currentPlayer + 1);
+              setHintVisibility(false);
+              setLocationVisibility(true);
+            }
+          }}
+        >
+          Ok
+        </Button>
+      </Modal>
+      <Modal isOpen={timerIsOpen} className="text-center">
+        <ModalBody>Время пошло!</ModalBody>
       </Modal>
     </Container>
   );
